3d71fb7a4eb65996e9b41526520de2c4a6bd525a
Radi <radi.rusadi.s@gmail.com>
2019-02-04 02:05:20 +0700

    Adding data and detail item products

--- src/DetailItem.js
+++ src/DetailItem.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
-import { Container, Content, Text } from 'native-base';
+import { Container, Content, Text, Thumbnail, Button } from 'native-base';
 
+// Components
 import AppBar from './components/AppBar';
 
+// Data Services
+import { getProduct } from './services/fakeProductServices';
+
 export default class DetailItem extends Component {
+  state = {
+    data: {
+      name: '',
+      imgUrl: 'kdkd',
+      price: '',
+      description: ''
+    }
+  };
+
+  componentDidMount() {
+    const { productId } = this.props;
+    const data = getProduct(productId);
+
+    this.setState({ data });
+  }
+
   render() {
+    const { name, imgUrl, price, description } = this.state.data;
+
     return (
       <Container>
         <AppBar />
         <Content>
-          <Text>{this.props.productId}</Text>
+          <Thumbnail
+            square
+            source={{ uri: imgUrl }}
+            style={{ height: 200, width: null, flex: 1 }}
+          />
+          <Text>{name}</Text>
+          <Text>{price}</Text>
+          <Text>{description}</Text>
+          <Button block>
+            <Text>TEst</Text>
+          </Button>
         </Content>
       </Container>
     );
